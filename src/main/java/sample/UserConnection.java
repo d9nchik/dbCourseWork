396eca6d824e06514a514be25e7e86820d982a80
d9nich <dan.galaiko@gmail.com>
2020-10-27 19:38:46 +0200

    Added UserConnection class.

---
+++ src/main/java/sample/UserConnection.java
@@ -0,0 +1,40 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UserConnection {
+    private Connection connection;
+
+    public UserConnection() {
+    }
+
+    public void connect(String login, String password) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost/courseWork",
+                    login, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isConnected() {
+        try {
+            return connection != null && !connection.isClosed();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return false;
+    }
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        connection = null;
+    }
+}
---
+++ src/main/webapp/employees.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: d9nich
+  Date: 27.10.2020
+  Time: 19:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Employees</title>
+</head>
+<body>
+
+</body>
+</html>
