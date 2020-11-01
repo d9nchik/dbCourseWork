b28f4b65a7b0bfaf485755ded094099afef65f94
d9nich <dan.galaiko@gmail.com>
2020-11-01 23:10:06 +0200

    Now i can hire new employee

---
+++ src/main/java/sample/HireEmployee.java
@@ -0,0 +1,64 @@
+package sample;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HireEmployee {
+    public Map<Integer, String> getListOfEmployees(UserConnection connection) {
+        Map<Integer, String> integerStringMap = new HashMap<>();
+        try {
+            ResultSet resultSet = connection.getConnection().createStatement().executeQuery(
+                    "SELECT \"EmployeeID\", \"LastName\", \"FirstName\", \"Title\"\n" +
+                            "FROM \"courseWork\".public.employees WHERE \"HireDate\" IS NOT NULL;");
+            while (resultSet.next()) {
+                String label = resultSet.getString(2) + " " + resultSet.getString(3) + " ("
+                        + resultSet.getString(4) + ")";
+                integerStringMap.put(resultSet.getInt(1), label);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return integerStringMap;
+    }
+
+    public boolean registerEmployee(UserConnection userConnection, String lastname, String firstname, String title,
+                                    String birthdate, String hireDate, String address, String city, String region,
+                                    String postalCode, String mobilePhone, String notes,
+                                    String reportsTo) {
+        if (lastname.length() > 20 || firstname.length() > 10 || title.length() > 30 || address.length() > 60
+                || region.length() > 15 || postalCode.length() > 10 || mobilePhone.length() > 24) return false;
+        try {
+            PreparedStatement preparedStatement = userConnection.getConnection().prepareStatement(
+                    "INSERT INTO \"courseWork\".public.employees ( \"LastName\", \"FirstName\", \"Title\"," +
+                            " \"BirthDate\", \"HireDate\", \"Address\", \"City\",\"Region\", \"PostalCode\"," +
+                            "\"Country\", \"MobilePhone\", \"Notes\", \"ReportsTo\")\n" +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, 'Ukraine', ?, ?, ?);");
+            Date birthDate = new SimpleDateFormat("yyyy-MM-dd").parse(birthdate);
+            Date hire = new SimpleDateFormat("yyyy-MM-dd").parse(hireDate);
+            int reportsToId = Integer.parseInt(reportsTo);
+            preparedStatement.setString(1, lastname);
+            preparedStatement.setString(2, firstname);
+            preparedStatement.setString(3, title);
+            preparedStatement.setDate(4, new java.sql.Date(birthDate.getTime()));
+            preparedStatement.setDate(5, new java.sql.Date(hire.getTime()));
+            preparedStatement.setString(6, address);
+            preparedStatement.setString(7, city);
+            preparedStatement.setString(8, region);
+            preparedStatement.setString(9, postalCode);
+            preparedStatement.setString(10, mobilePhone);
+            preparedStatement.setString(11, notes);
+            preparedStatement.setInt(12, reportsToId);
+            preparedStatement.execute();
+        } catch (SQLException | ParseException throwables) {
+            System.out.println(throwables.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/webapp/hireEmployee.jsp
@@ -0,0 +1,99 @@
+<%@ page import="java.util.Map" %><%--
+  Created by IntelliJ IDEA.
+  User: d9nich
+  Date: 01.11.2020
+  Time: 21:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<jsp:useBean id="registerEmployee" scope="application" class="sample.HireEmployee"/>
+<jsp:useBean id="connection" scope="session" class="sample.UserConnection"/>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hire employee</title>
+</head>
+<body>
+<%if (!connection.isConnected()) {%>
+<jsp:forward page="login.jsp"/>
+<%}%>
+<% if (request.getParameterMap().size() == 12 && registerEmployee.registerEmployee(connection,
+        request.getParameter("lastname"), request.getParameter("firstname"), request.getParameter("title"),
+        request.getParameter("birthdate"), request.getParameter("hireDate"), request.getParameter("address"),
+        request.getParameter("city"), request.getParameter("region"), request.getParameter("postalCode"),
+        request.getParameter("mobilePhone"), request.getParameter("notes"), request.getParameter("reportTo"))) {%>
+<h1>Successfully registered new employee</h1>
+<% } else { %>
+<form method="post">
+    <label>
+        Lastname:
+        <input type="text" maxlength="20" name="lastname" required>
+    </label><br>
+    <label>
+        Firstname:
+        <input type="text" maxlength="10" name="firstname" required>
+    </label><br>
+
+    <label>
+        Title:
+        <input type="text" maxlength="30" name="title">
+    </label><br>
+    <label>
+        Birthdate:
+        <input type="date" name="birthdate">
+    </label><br>
+    <label>
+        Hire date:
+        <input type="date" name="hireDate">
+    </label><br>
+
+    <label>
+        Address:
+        <input type="text" maxlength="60" name="address">
+    </label><br>
+
+    <label>
+        City:
+        <input type="text" maxlength="15" name="city">
+    </label><br>
+
+    <label>
+        Region:
+        <input type="text" maxlength="15" name="region">
+    </label><br>
+
+    <label>
+        Postal code:
+        <input type="text" maxlength="10" name="postalCode">
+    </label><br>
+
+
+    <label>
+        Mobile Phone:
+        <input type="text" maxlength="24" name="mobilePhone">
+    </label><br>
+
+    <label>
+        Notes:
+        <textarea name="notes"></textarea>
+    </label>
+
+    <label>
+        Reports to:
+        <select name="reportTo">
+            <%
+                Map<Integer, String> integerStringMap = registerEmployee.getListOfEmployees(connection);
+
+                for (Integer key : integerStringMap.keySet()) {
+            %>
+            <option value="<%=key%>"><%=integerStringMap.get(key)%>
+            </option>
+            <%}%>
+        </select>
+    </label>
+
+    <input type="submit" value="Submit">
+</form>
+<%}%>
+</body>
+</html>
